be151da44d7d3ecd7568d4985b173ffa55a6a689
masaun <unomasanori@unomasanori-no-MacBook-Air.local>
2016-10-09 22:53:28 +0900

    AngularJS フォーム作成 validationとerror message

---
+++ myscript.js
@@ -0,0 +1,18 @@
+angular.module('myapp', [])
+    .controller('MainController', ['$scope', function($scope) {
+        $scope.users = [
+            {"name":"taguchi", "score":52.22},
+            {"name":"tanaka", "score":38.22},
+            {"name":"yamada", "score":11.11},
+            {"name":"hayashi", "score":5.25},
+            {"name":"tanahashi", "score":82.4},
+            {"name":"yasuda", "score":55.21},
+            {"name":"minami", "score":32.8},
+            {"name":"yanagi", "score":72.2}
+        ];
+    }])
+    .controller('UserItemController', ['$scope', function($scope) {
+        $scope.increment = function() {
+            $scope.user.score++;
+        };
+    }]);
